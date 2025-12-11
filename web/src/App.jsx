cda24f91256f61cbf784fd889c510820662cba5b
Matthew Samar <mattwsamar@gmail.com>
2025-12-10 23:13:11 -0800

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Dashboard from './pages/Dashboard';
+import Sidebar from './components/sidebar';
+import Dashboard from './pages/dashboard';
 
 function App() {
   return (
