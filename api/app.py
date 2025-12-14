9e08730c8033fb75ea27543548632080cd0d1937
cooljeffman <jeffrey.reyes02@student.csulb.edu>
2025-12-13 17:53:58 -0800

    Refactor app initialization and blueprint registration

--- api/app.py
+++ api/app.py
@@ -1,7 +1,30 @@
+from flask import Flask
+from flask_jwt extended import JWTManager
+from api.config import Config
+from api.db import Base, engine
+
+#Import Blueprints
 from api.routes.auth import auth_bp
-app.register_blueprint(auth_bp)
 from api.routes.users import user_bp
-app.register_blueprint(users_bp)
 from api.routes.admin import admin_bp
-app.register_blueprint(admin_bp)
+
+def create_app():
+  app = Flask(__name__)
+  app.config.from_object(Config)
+
+  JMTManager(app)
+
+  Base.metadata.create_all(bind=engine)
+
+  app.register_blueprint(auth_bp)
+  app.register_blueprint(users_bp)
+  app.register_blueprint(admin_bp)
+
+  return app
+
+app = create_app()
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
 
